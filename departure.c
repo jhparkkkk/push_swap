26892e55fb2fabe82f67077ed7c0201793a5f7e0
Jee hyun PARK <jeepark@e3r13p6.clusters.42paris.fr>
2022-03-06 20:17:46 +0100

    < 5500

--- departure.c
+++ departure.c
@@ -6,20 +6,84 @@
 /*   By: jeepark <jeepark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 17:42:53 by jeepark           #+#    #+#             */
-/*   Updated: 2022/03/04 19:10:38 by jeepark          ###   ########.fr       */
+/*   Updated: 2022/03/06 20:14:20 by jeepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	control_visa(t_toolbox *box)
+int	ft_lstlen_b(t_toolbox *box)
+{
+	t_list_int	*tmp;
+	int			i;
+
+	i = 0;
+	tmp = box->list_b;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	i++;
+	return (i);
+}
+
+static int  *ft_tab_sort(t_toolbox *box)
+{
+    int i;
+    int j;
+    int tmp;
+	int *copy;
+    i = 0;
+	copy = ft_calloc(box->count, sizeof(int));
+	while (i < box->count)
+	{
+		copy[i] = box->values[i];
+		i++;
+	}
+	i = 0;
+	j = 0;
+    while (i < box->count)
+    {
+        j = i + 1;
+        while (j < box->count)
+        {
+            if (copy[i] > copy[j])
+            {
+                tmp = copy[i];
+                copy[i] = copy[j];
+                copy[j] = tmp;
+            }
+            j++;
+        }
+        i++;
+	}
+	/*i = 0;
+	while(copy[i])
+	{
+		printf("copy[%d] = %d\n", i, copy[i]);
+		i++;
+	}*/
+	return(copy);                     
+}
+
+static int	find_mediane(t_toolbox *box)
+{
+	int mediane =  box->count / 2;
+	int *sorted_tab;
+	sorted_tab = ft_tab_sort(box);
+	//printf("MEDIANE = %d\n", sorted_tab[mediane]);
+	return(sorted_tab[mediane]);
+}
+
+int	control_visa(t_toolbox *box, t_list_int *tmp)
 {
 	int	valid_visa;
 
 	valid_visa = 0;
 	while (valid_visa < box->max)
 	{
-		if (box->list_a->content == box->lis[valid_visa])
+		if (tmp->content == box->lis[valid_visa])
 			return (0);
 		valid_visa++;
 	}
@@ -28,23 +92,45 @@ int	control_visa(t_toolbox *box)
 
 t_toolbox	boarding_gate(t_toolbox *box)
 {
-	int	passenger;
+	//int	passenger;
 	int	scanning;
-
-	passenger = 0;
+	t_list_int *tmp;
+	int	mediane;
+	int compteur;
+	//passenger = 0;
 	scanning = box->count;
-	while (passenger <= scanning)
+	tmp = box->list_a;
+	mediane = find_mediane(box);
+
+	compteur = box->count - box->max;
+	//printf("MEDIANE = %d\n", mediane);
+	while (scanning > 0 || compteur > 0)
 	{
-		if (control_visa(box) == 0)
+		//printf("PASSENGER %d = %d\n", scanning, tmp->content);
+		if (control_visa(box, tmp) == 0)
 		{
+			
 			ft_rotate_a(box);
 			write(1, "ra\n", 3);
 		}
 		else
 		{
-			ft_push_b(box);
+			if (tmp->content < mediane)
+			{
+				//printf("coucou\n");
+				ft_push_b(box);
+				if (ft_lstlen_b(box) > 1)
+				{
+					ft_rotate_b(box);
+					write (1, "rb\n", 3);
+				}
+			}
+			else
+				ft_push_b(box);
+			compteur--;
 		}
-		passenger++;
+		tmp = box->list_a;
+		scanning--;
 	}
 	box->count -= box->max;
 	return (*box);
--- push_swap.c
+++ push_swap.c
@@ -6,12 +6,27 @@
 /*   By: jeepark <jeepark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 18:07:02 by jeepark           #+#    #+#             */
-/*   Updated: 2022/03/05 21:54:42 by jeepark          ###   ########.fr       */
+/*   Updated: 2022/03/06 19:49:54 by jeepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "push_swap.h"
+
+void print_list(t_list_int *list)
+{
+	t_list_int  *tmp = list;
+	if (!list)
+	{    
+		return ;
+	}
+	while (tmp->next != NULL)
+	{
+		printf("%d\n", tmp->content);
+		tmp = tmp->next;
+	}
+	printf("%d\n", tmp->content);
+}
 
 int	main(int ac, char **av)
 {
@@ -20,6 +35,13 @@ int	main(int ac, char **av)
 
 	ft_check_error(av, ac);
 	ft_toolbox_init(&box, ac, av);
+	
+	/*i = 0;
+	while(i < box.max)
+	{
+		printf("LIS[%d] = %d\n", i, box.lis[i]);
+		i++;
+	}*/
 	if (ac <= 2)
 		return (free(box.values), ft_list_free(&box), 0);
 	if (ft_check_sorted_list_a(&box) == 0)
@@ -31,6 +53,7 @@ int	main(int ac, char **av)
 	if (ac > 6 || ac == 5)
 	{
 		boarding_gate(&box);
+		//print_list(box.list_a);
 		i = box.count + 1;
 		while (--i >= 0)
 			meta_simulation(&box);
@@ -38,5 +61,6 @@ int	main(int ac, char **av)
 			fasten_your_seatbelt(&box);
 		free(box.lis);
 	}
+	//print_list(box.list_a);
 	return (free(box.values), ft_list_free(&box), 0);
 }
